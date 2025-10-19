9ce0340e26273bb53ca1015923eff3fad319a006
Nasim Ahmad <khannasimahmad@gmail.com>
2025-10-19 08:44:32 +0530

    add server filer to test on local

---
+++ test-server.js
@@ -0,0 +1,13 @@
+// import express from 'express';
+// import transactionRouter from './route/transaction.router.js';
+// const app = express();
+// // JSON parser
+// app.use(express.json());
+// // Mount router at /api
+// app.use('/api', transactionRouter);
+// // Root route
+// app.get('/', (req, res) => res.send('Stock Backend Lambda running!'));
+// app.listen(2024, () => {
+//     console.log('Server is running on port 3000');
+// });
+
