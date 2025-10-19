e284a8b441d7bba328b2028506cb10596dc3e37b
Nasim Ahmad <khannasimahmad@gmail.com>
2025-10-19 12:22:29 +0530

    added route for saving and listing the stocks results

--- route/transaction.router.js
+++ route/transaction.router.js
@@ -1,12 +1,13 @@
 import { Router } from 'express';
 import { getLastProcessed, buy, sell, updateLastProcessed } from '../controller/db.transaction.controller.js';
-
+import { saveStock, listStocks } from '../controller/dynamoDBController.js';
 const router = Router();
 
 router.get('/last-processed', getLastProcessed);
 router.patch('/last-processed', updateLastProcessed);
 router.post('/buy', buy);
 router.post('/sell', sell);
-router.post('/dump')
+router.post('/save-result', saveStock);
+router.get('/list-result', listStocks);
 
 export default router;
