d752cd3df60fa126697fecbf3094914dd3628577
Nasim Ahmad <khannasimahmad@gmail.com>
2025-11-08 19:52:04 +0530

    added route for setting and getting the fcm tokens

--- route/transaction.router.js
+++ route/transaction.router.js
@@ -1,6 +1,7 @@
 import { Router } from 'express';
 import { getLastProcessed, buy, sell, updateLastProcessed, listStocks, topGrowers } from '../controller/db.transaction.controller.js';
 import { saveStock } from '../controller/dynamoDBController.js';
+import { saveFcmToken, getAllFcmTokens } from '../controller/user.controller.js'
 
 const router = Router();
 
@@ -11,5 +12,7 @@ router.post('/sell', sell);
 router.post('/save-result', saveStock);
 router.get('/list-result', listStocks);
 router.get('/top-growers', topGrowers);
+router.post("/save-fcm-token", saveFcmToken);
+router.get("/list-fcm-tokens", getAllFcmTokens);
 
 export default router;
